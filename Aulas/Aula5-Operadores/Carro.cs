15209d1fcf647a62f94bc8733c4d6eab533da2f4
lufer <lufer@ipca.pt>
2021-10-25 17:04:39 +0100

    Overriding e Operators

---
+++ Aulas/Aula5-Operadores/Carro.cs
@@ -0,0 +1,114 @@
+﻿/*
+*	<copyright file="Aula5_Operadores.cs" company="IPCA">
+*		Copyright (c) 2021 All Rights Reserved
+*	</copyright>
+* 	<author>lufer</author>
+*   <date>10/25/2021 3:06:57 PM</date>
+*	<description>
+*   Operadores
+*   Equals
+*   </description>
+**/
+using System;
+
+namespace Aula5_Operadores
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: lufer
+    /// Created on: 10/25/2021 3:06:57 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Carro
+    {
+        #region Attributes
+        string matricula;
+        int ano;
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Carro()
+        {
+            matricula = "";
+            ano = 2021;
+        }
+
+        public Carro(int ano, string matricula)
+        {
+            this.matricula = matricula;
+            this.ano = ano;
+        }
+
+        #endregion
+
+        #region Properties
+        public string Matricula
+        {
+            get { return matricula; }
+            set { if (value.Length > 0) matricula = value; }
+        }
+
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return "Matricula: " + matricula + " Ano: " + ano.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            //return base.Equals(obj);
+            //Carro aux = obj as Carro;
+            Carro aux = (Carro)obj;
+
+            if (aux.Ano == this.Ano && aux.Matricula == this.Matricula)
+                return true;
+            return false;
+        }
+        #endregion
+
+        #region OtherMethods
+
+        #region Operadores
+
+        public static bool operator ==(Carro a, Carro b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Carro a, Carro b)
+        {
+            return !(a==b);
+        }
+
+        #endregion
+
+
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Carro()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Aulas/Aula5-Operadores/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+*	<copyright file="Aula5_Operadores.cs" company="IPCA">
+*		Copyright (c) 2021 All Rights Reserved
+*	</copyright>
+* 	<author>lufer</author>
+*   <date>10/25/2021 3:06:57 PM</date>
+*	<description>
+*       Manipular Operadores
+*       Reescrever Equals
+*   </description>
+**/
+
+using System;
+
+namespace Aula5_Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Carro c1 = new Carro(2021, "00-00-00");
+            Carro c2 = new Carro(2021, "00-00-00");
+
+            //usa ToString
+            Console.WriteLine(c1.ToString());
+            //usa operador ==
+            Console.WriteLine("C1 == C2: " + (c1 == c2).ToString());
+            //Usa Equals
+            Console.WriteLine("C1 Equals C2: " + (c1.Equals(c2)).ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
